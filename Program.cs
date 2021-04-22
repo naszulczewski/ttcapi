c4b38d2ff8aec32e9f7a2b2135ea68fd752dde6f
Natasha Szulczewski <natashaszulczewski@Natashas-MacBook-Pro.local>
2021-04-22 10:25:12 -0500

    good morning

--- Program.cs
+++ Program.cs
@@ -22,8 +22,8 @@ namespace API
             // seedData seed = new seedData();
             // seed.SeedData();
 
-            seedOrderTable seedOrder = new seedOrderTable();
-            seedOrder.seedOrderTableData();
+            // seedOrderTable seedOrder = new seedOrderTable();
+            // seedOrder.seedOrderTableData();
 
             CreateHostBuilder(args).Build().Run();
         }
