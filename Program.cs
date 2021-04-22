7846932d83232bb6376eab450ca9f04358f0b9a4
Natasha Szulczewski <natashaszulczewski@Natashas-MacBook-Pro.local>
2021-04-22 11:08:22 -0500

    cart table

--- Program.cs
+++ Program.cs
@@ -19,14 +19,14 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            // seedData seed = new seedData();
-            // seed.SeedData();
+            seedData seed = new seedData();
+            seed.SeedData();
 
             // seedOrderTable seedOrder = new seedOrderTable();
             // seedOrder.seedOrderTableData();
 
-            seedTotalData seedie = new seedTotalData();
-            seedie.SeedData();
+            // seedTotalData seedie = new seedTotalData();
+            // seedie.SeedData();
 
             CreateHostBuilder(args).Build().Run();
         }
