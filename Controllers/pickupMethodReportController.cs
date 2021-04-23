19a8e0cdbc1e94a01033288eb74abcf617de1251
Natasha Szulczewski <natashaszulczewski@Natashas-MacBook-Pro.local>
2021-04-23 01:27:53 -0500

    pickup report

--- Controllers/pickupMethodReportController.cs
+++ Controllers/pickupMethodReportController.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using API.Reports;
+using Microsoft.AspNetCore.Cors;
+using API.TotalCartFunctions;
+using API.Cartfunctions;
+using ttcapi.orderIDCreation;
+using ttcapi.Reports;
+using API;
 
 namespace API.Controllers
 {
@@ -12,32 +19,37 @@ namespace API.Controllers
     public class pickupMethodReportController : ControllerBase
     {
         // GET: api/pickupMethodReport
+        [EnableCors("AnotherPolicy")]
         [HttpGet]
-        public IEnumerable<string> Get()
+        public List<orderevent> Get()
         {
-            return new string[] { "value1", "value2" };
+            PickupMethod pickup = new  PickupMethod();
+            return pickup.PickupMethodReport();
         }
 
-        // GET: api/pickupMethodReport/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        // // GET: api/pickupMethodReport/5
+        // [HttpGet("{id}", Name = "Get")]
+        // public string Get(int id)
+        // {
+        //     return "value";
+        // }
 
         // POST: api/pickupMethodReport
+        [EnableCors("AnotherPolicy")]
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
         // PUT: api/pickupMethodReport/5
+        [EnableCors("AnotherPolicy")]
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE: api/pickupMethodReport/5
+        [EnableCors("AnotherPolicy")]
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
--- Reports/PickupMethod.cs
+++ Reports/PickupMethod.cs
@@ -1,7 +1,39 @@
+
+
+using System.Collections.Generic;
+using API;
+using MySql.Data.MySqlClient;
+
 namespace ttcapi.Reports
 {
     public class PickupMethod
     {
-        
+        public List<orderevent> PickupMethodReport()
+        {
+            List<orderevent> cartTotals = new List<orderevents>();
+
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            string stm = "CREATE VIEW PickupOrders AS SELECT COUNT( SELECT fulfilledStatus FROM orderevent WHERE fulfilledStatus == 1);";
+            using var cmd = new MySqlCommand(stm, con);
+
+            cmd.ExecuteNonQuery();
+
+            string stm1 = "CREATE VIEW DeliveryOrders AS SELECT COUNT( SELECT fulfilledStatus FROM orderevent WHERE fulfilledStatus == 2);";
+            using var cmd1 = new MySqlCommand(stm1, con);
+
+            cmd1.ExecuteNonQuery();
+
+            string stm2 = "SELECT TOP 1 COUNT(fulfilledStatus), OrderID FROM PickupOrders, DeliveryOrders GROUP BY OrderID, fulfilledStatus ORDER BY fulfilledStatus asc;";
+            using var cmd2 = new MySqlCommand(stm2, con);
+
+            cmd2.ExecuteNonQuery();
+
+            return cartTotals;
+        }
     }
 }
