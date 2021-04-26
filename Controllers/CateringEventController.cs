036c9e60eb101f21986d6d4b7ef52fa0e53d5908
Sydney Bonner <sebonner1@crimson.ua.edu>
2021-04-26 11:58:07 -0500

    working on calendar

--- Controllers/CateringEventController.cs
+++ Controllers/CateringEventController.cs
@@ -38,7 +38,7 @@ namespace API.Controllers
         // POST: api/CateringEvent
         [EnableCors("AnotherPolicy")]
         [HttpPost]
-        public void Post([FromBody] CateringEvent totalEvents)
+        public void Post([FromBody] List<CateringEvent> totalEvents)
         {
             Console.WriteLine(totalEvents);
             // putEventTotals putEvent = new putEventTotals();
